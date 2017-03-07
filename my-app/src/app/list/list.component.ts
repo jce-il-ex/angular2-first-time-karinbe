eb6861bf9e766b86b8ecf7f5da622a25832bcf3f
karinbe <karinbenson8@gmail.com>
2017-03-07 13:07:26 +0200

    test

---
+++ my-app/src/app/list/list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+MyList = [
+
+{id:1
+},
+{id:2
+},
+{id:3
+},
+{id:4
+},
+];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
